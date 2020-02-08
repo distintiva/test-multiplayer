9363c63bc08d0294bb7aa98eb076551a15f381ae
Distintiva Solutions <57623220+distintiva@users.noreply.github.com>
2020-02-08 12:08:56 +0100

    Update main.blocks, multiplayer.ts

--- multiplayer.ts
+++ multiplayer.ts
@@ -5,7 +5,39 @@
 //% weight=0 color=#b8860b icon="\uf021" block="Sprite Transforms"
 //% advanced=true
 namespace multiplayer {
+    enum GameMessage {
+        Update = 7,
+        LaserCreated = 8,
+        AsteroidCreated = 9,
+        AsteroidDestroyed = 10,
+        HudUpdate = 11
+    }
 
+    const socket = multiplayer.Socket.getInstance();
+
+    enum ProgramState {
+        Waiting = 0,
+        Counting = 1,
+        Playing = 2,
+        Disconnected = 3
+    }
+
+    let funcOnConnected: () => void;
+
+    let waitTitle = "", waitSubtitle = "", waitTitleColor = 1;
+
+    let pl1: Sprite, pl2: Sprite;
+    let _vx: number = 100, _vy: number = 100;
+
+    let programState = ProgramState.Waiting;
+
+    let gameStarted = false;
+
+    const dbFont = image.doubledFont(image.font8);
+
+    let offset = 0;
+    let flip = true;
+    let readyCount = 3000;
 
     //% blockId=transform_change_rotation
     //% block="start mutiplayer game"
@@ -70,32 +102,66 @@ namespace multiplayer {
         waitForOtherPlayer();
     })
 
+    game.onUpdateInterval(100, () => {
+        if (programState == ProgramState.Playing){
+            sendPlayerState();
+        }
+        //this.sendPlayerState();
+    });
 
-const socket = multiplayer.Socket.getInstance();
 
-enum ProgramState {
-    Waiting = 0,
-    Counting = 1,
-    Playing = 2,
-    Disconnected = 3
-}
+    function sendPlayerState(kind = GameMessage.Update, arg = 0) {
+        const playerSprite = isPlayerOne() ? pl1 : pl2;
+        const packet = new SocketPacket();
+        packet.arg1 = kind;
+        packet.arg2 = playerSprite.x;
+        packet.arg3 = playerSprite.y;
+        packet.arg4 = playerSprite.vx;
+        packet.arg5 = playerSprite.vy;
 
-let funcOnConnected: () => void;
+        socket.sendCustomMessage(packet);
 
-let waitTitle ="", waitSubtitle = "", waitTitleColor = 1;
+        /*if (isPlayerOne()) {
+            this.sendHUD();
+        }*/
+    }
 
-let pl1:Sprite, pl2:Sprite;
-let _vx:number=100, _vy:number=100;
+    function updatePlayerState(packet: SocketPacket) {
+        const otherSprite = isPlayerOne() ? pl2 : pl1;
+        otherSprite.x = packet.arg2;
+        otherSprite.y = packet.arg3;
+        otherSprite.vx = packet.arg4;
+        otherSprite.vy = packet.arg5;
+    }
 
-let programState = ProgramState.Waiting;
+    socket.onMessage((packet: SocketPacket) => {
+        switch (packet.arg1) {
+            case GameMessage.Update:
+                updatePlayerState(packet);
+                break;
+            /*case GameMessage.LaserCreated:
+                this.updatePlayerState(packet);
+                if (isPlayerOne()) {
+                    this.createLaser(SpriteKindLegacy.Laser2, this.ship2, packet.arg5);
+                }
+                else {
+                    this.createLaser(SpriteKindLegacy.Laser1, this.ship1, packet.arg5);
+                }
+                break;
+            case GameMessage.AsteroidCreated:
+                this.createAsteroid(packet.arg2, packet.arg3)
+                break;
+            case GameMessage.AsteroidDestroyed:
+                this.handleCollision(packet.arg2, packet.arg3, packet.arg4 === 1)
+                break;
+            case GameMessage.HudUpdate:
+                this.updateHUD(packet);
+                break;*/
+        }
+    })
 
-let gameStarted = false;
 
-const dbFont = image.doubledFont(image.font8);
 
-let offset = 0;
-let flip = true;
-let readyCount = 3000;
 
 
 
